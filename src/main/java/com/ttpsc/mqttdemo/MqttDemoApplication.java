9afdaa7593b6a5260a1bbaa27aa2144619524168
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-03-31 16:42:58 +0200

    initial commit

---
+++ src/main/java/com/ttpsc/mqttdemo/MqttDemoApplication.java
@@ -0,0 +1,81 @@
+package com.ttpsc.mqttdemo;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+@SpringBootApplication
+public class MqttDemoApplication {
+
+    private static String MESSAGE;
+    private final String HOST;
+    private final Integer PORT;
+    private final String DEFAULT_TOPIC;
+    private final String CLIENT_ID;
+    private final String USERNAME;
+    private final String PASSWORD;
+
+    public MqttDemoApplication(@Value("${mqtt.message}") String message, @Value("${mqtt.hostname}") String host, @Value("${mqtt.port}") Integer port,
+                               @Value("${mqtt.topic}") String defaultTopic, @Value("${mqtt.clientId}") String clientId, @Value("${mqtt.username}") String username,
+                               @Value("${mqtt.password}") String password) {
+        this.MESSAGE = message;
+        this.HOST = host;
+        this.PORT = port;
+        this.DEFAULT_TOPIC = defaultTopic;
+        this.CLIENT_ID = clientId;
+        this.USERNAME = username;
+        this.PASSWORD = password;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(MqttDemoApplication.class)
+                .run(args);
+
+        MyGateway gateway = context.getBean(MyGateway.class);
+        gateway.sendToMqtt(MESSAGE);
+    }
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[]{"tcp://" + HOST + ":" + PORT});
+//        options.setUserName(USERNAME);
+//        options.setPassword(PASSWORD.toCharArray());
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutbound() {
+        MqttPahoMessageHandler messageHandler =
+                new MqttPahoMessageHandler(CLIENT_ID, mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic(DEFAULT_TOPIC);
+        return messageHandler;
+    }
+
+    @Bean
+    public MessageChannel mqttOutboundChannel() {
+        return new DirectChannel();
+    }
+
+    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
+    public interface MyGateway {
+
+        void sendToMqtt(String data);
+
+    }
+}
---
+++ src/test/java/com/ttpsc/mqttdemo/MqttDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttpsc.mqttdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
